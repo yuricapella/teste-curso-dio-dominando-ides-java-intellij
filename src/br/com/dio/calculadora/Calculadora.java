042989e3efa8cc8fc92dc94654274f35a1c08757
yuricapella <yuricapelladossantos@gmail.com>
2022-02-07 20:36:09 -0300

    criando calculadora com ,

---
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,43 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a, b;
+
+        System.out.println("Digite o primeiro valor: ");
+        a = scan.nextInt();
+        System.out.println("Digite o segundo valor: ");
+        b = scan.nextInt();
+
+        int somar = somar(a, b);
+        int subtrair = subtrair(a, b);
+        int multiplicar = multiplicar(a, b);
+        Double dividir = dividir(Double.valueOf(a), Double.valueOf(b));
+
+        System.out.println("somar: " + somar);
+        System.out.println("subt: " + subtrair);
+        System.out.println("mult: " + multiplicar);
+        System.out.println("div: " + dividir);
+    }
+
+    public static int somar(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtrair(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public static Double dividir(Double a, Double b) {
+        return a / b;
+    }
+
+}
